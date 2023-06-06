e9c432e5ac29c5766ca0f5c06498e7f26db4cc6a
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-06-05 23:36:07 -0300

    feat: creating basic user table

---
+++ app/models/userTable.py
@@ -0,0 +1,20 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+
+    def __init__(self, name):
+        self.name = name
+    
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "name": self.name
+        }
+        return data
+    
+    def __repr__(self):
+        return "<User %r %r>" % self.id, self.name
