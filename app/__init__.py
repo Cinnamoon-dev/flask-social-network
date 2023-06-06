9877e48f5839e128f475aa98d61f4a027a3c7b07
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-06-05 23:38:11 -0300

    feat: adding a db conn and a migration instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object("config")
 
-from app.controllers import testController
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app.models.userTable import User
+
+from app.controllers import userController
