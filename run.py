a9157e28c23e0b1082c8b71ae22af0999e4127e4
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-06-08 17:06:21 -0300

    feat: adding run on all addresses

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.getenv('PORT', 4444)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv('PORT', 4444)))
