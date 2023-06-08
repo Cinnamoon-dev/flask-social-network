30d61412a2b536452248d9fd2c8b0e68ec8db5da
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-06-07 22:31:15 -0300

    feat: adding an email column in the db

--- app/models/userTable.py
+++ app/models/userTable.py
@@ -4,17 +4,20 @@ class User(db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
 
-    def __init__(self, name):
+    def __init__(self, name, email):
         self.name = name
+        self.email = email
     
     def to_dict(self):
         data = {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "email": self.email
         }
         return data
     
     def __repr__(self):
-        return "<User %r %r>" % self.id, self.name
+        return "<User %r %r %r>" % self.id, self.name, self.email
