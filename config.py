cf615eb5a0f56e1f929e08f3343d0fe054b2c746
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-07-06 22:15:23 -0300

    refactor: creating a dict for using many db types

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///storage.db"
+USER = "postgres"
+PASSWORD = "1234"
+HOST = "localhost"
+DATABASE_NAME = "flask-social"
+
+DATABASE = {
+    "sqlite": "sqlite:///storage.db",
+    "postgres": f"postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
+}
+
+SQLALCHEMY_DATABASE_URI = DATABASE["sqlite"]
 SQLALCHEMY_TRACK_MODIFICATIONS = True
