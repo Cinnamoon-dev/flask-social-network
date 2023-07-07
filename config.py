63a5b180a8c10475f2c9ff78b9bd898abc04e444
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-07-07 13:02:59 -0300

    fix: adding a port to the postgres db

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ DEBUG = True
 USER = "postgres"
 PASSWORD = "1234"
 HOST = "localhost"
+PORT = "5432"
 DATABASE_NAME = "flask-social"
 
 DATABASE = {
     "sqlite": "sqlite:///storage.db",
-    "postgres": f"postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
+    "postgres": f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE_NAME}"
 }
 
 SQLALCHEMY_DATABASE_URI = DATABASE["sqlite"]
