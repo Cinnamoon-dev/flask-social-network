2303bcb3948c8a8233ce0d2d9e458c65c6f158bc
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-07-20 11:18:14 -0300

    feat: creating password column

--- app/models/userTable.py
+++ app/models/userTable.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash
 
 class User(db.Model):
     __tablename__ = "user"
@@ -6,18 +7,21 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
 
-    def __init__(self, name, email):
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
+        self.password = generate_password_hash(password, "sha256")
     
     def to_dict(self):
         data = {
             "id": self.id,
             "name": self.name,
-            "email": self.email
+            "email": self.email,
+            "password": self.password
         }
         return data
     
     def __repr__(self):
-        return "<User %r %r %r>" % self.id, self.name, self.email
+        return "<User %r %r %r %r>" % self.id, self.name, self.email, self.password
