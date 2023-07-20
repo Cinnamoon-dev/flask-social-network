77e30ceb7518944d369f3eccda7f42f5a26efd51
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-07-20 10:55:12 -0300

    chore: changing database to postgres

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 DEBUG = True
 
 USER = "postgres"
-PASSWORD = "1234"
+PASSWORD = "postgres"
 HOST = "localhost"
-PORT = "5432"
-DATABASE_NAME = "flask-social"
+PORT = "5433"
+DATABASE_NAME = "postgres"
 
 DATABASE = {
     "sqlite": "sqlite:///storage.db",
     "postgres": f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE_NAME}"
 }
 
-SQLALCHEMY_DATABASE_URI = DATABASE["sqlite"]
+SQLALCHEMY_DATABASE_URI = DATABASE["postgres"]
 SQLALCHEMY_TRACK_MODIFICATIONS = True
