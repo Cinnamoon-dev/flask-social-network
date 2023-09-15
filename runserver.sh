fceed46f02ff7a58d979d99cae2cc50d424caa33
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-09-14 23:24:50 -0300

    feat: adding database recreation

--- runserver.sh
+++ runserver.sh
@@ -2,9 +2,12 @@
 
 if [ -d ./migrations ];
 then
+    psql -U postgres psql -c "DROP DATABASE postgres;"
     rm -rf migrations
     echo "Migrations Deleted"
 fi
+
+psql -U postgres psql -c "CREATE DATABASE postgres;"
 
 flask db init &&
 flask db migrate &&
