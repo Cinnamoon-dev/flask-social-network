ba63137e487df3cd56ceb0f797a9bf1d6665edca
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-09-16 15:46:17 -0300

    fix: removing parameters from database changes

--- runserver.sh
+++ runserver.sh
@@ -2,12 +2,12 @@
 
 if [ -d ./migrations ];
 then
-    psql -U postgres psql -c "DROP DATABASE postgres;"
+    psql -U postgres -c "DROP DATABASE postgres;"
     rm -rf migrations
     echo "Migrations Deleted"
 fi
 
-psql -U postgres psql -c "CREATE DATABASE postgres;"
+psql -U postgres -c "CREATE DATABASE postgres;"
 
 flask db init &&
 flask db migrate &&
