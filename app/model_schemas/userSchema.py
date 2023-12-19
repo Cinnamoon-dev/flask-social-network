9a3e11645edfec2e53e1d1fe7b77767233ca9ed1
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-19 09:58:09 -0300

    feat: creating new schemas for User validation

--- app/model_schemas/userSchema.py
+++ app/model_schemas/userSchema.py
@@ -1,6 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel
 
-class UserSchema(BaseModel):
+
+class UserAddSchema(BaseModel):
     name: str
     email: str
     password: str
+
+class UserEditSchema(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
