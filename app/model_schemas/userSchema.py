1e0b52cfb7330a78f2e8b150d47d9730d12795a9
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-19 10:45:48 -0300

    feat: adding email regex

--- app/model_schemas/userSchema.py
+++ app/model_schemas/userSchema.py
@@ -1,5 +1,6 @@
+import re
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class UserAddSchema(BaseModel):
@@ -7,7 +8,23 @@ class UserAddSchema(BaseModel):
     email: str
     password: str
 
+    @field_validator("email")
+    @classmethod
+    def email_validator(cls, email):
+        email = email.lower()
+        if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email) is None:
+            raise ValueError('Insert a valid email')
+        return email
+
 class UserEditSchema(BaseModel):
     name: Optional[str] = None
     email: Optional[str] = None
     password: Optional[str] = None
+
+    @field_validator("email")
+    @classmethod
+    def email_validator(cls, email):
+        email = email.lower()
+        if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email) is None:
+            raise ValueError('Insert a valid email')
+        return email
