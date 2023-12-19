7b96eaf73ddadc197ad5acbfa8efa3e30bdc173e
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-19 09:41:14 -0300

    feat: creating paginate and jsonifying factory

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,21 +1,22 @@
 from functools import wraps
-from flask import make_response, request
+from flask_sqlalchemy import BaseQuery
 from pydantic import BaseModel, ValidationError
+from flask import make_response, request, jsonify
 from werkzeug.security import generate_password_hash
 
 
 class ResponseFactory:
     def successAction():
-        return make_response({"error": False, "message": "Action done successfully"}, 200)
+        return make_response(jsonify({"error": False, "message": "Action done successfully"}), 200)
 
     def databaseError():
-        return make_response({"error": True, "message": "Database error"}, 500)
+        return make_response(jsonify({"error": True, "message": "Database error"}), 500)
 
     def notFound(field: str):
-        return make_response({"error": True, "message": f"{field} not found"}, 404)
+        return make_response(jsonify({"error": True, "message": f"{field} not found"}), 404)
     
     def alreadyRegistered(field: str):
-        return make_response({"error": True, "message": f"{field} already registered"}, 409)
+        return make_response(jsonify({"error": True, "message": f"{field} already registered"}), 409)
 
 
 # update table line
@@ -43,8 +44,8 @@ def instance_update(instance, request_json):
     if "password" in request_json:
         setattr(instance, 'password', generate_password_hash(request_json.get("password"), method="sha256"))
 
-# TODO
-# Test decorator
+
+# pydantic validator
 def request_validator(ModelClass: BaseModel):
     def wrapper(f):
         @wraps(f)
@@ -59,3 +60,26 @@ def request_validator(ModelClass: BaseModel):
             return f(*args, **kwargs)
         return wrapped
     return wrapper
+
+
+# item pagination
+def paginate(query: BaseQuery, page: int=1, rows_per_page: int=1):
+
+    pagination = query.paginate(page=page, per_page=rows_per_page, error_out=False)
+
+    data = pagination.items
+
+    output = {
+        "pagination": {
+            "pages_count": pagination.pages,
+            "itens_count": pagination.total,
+            "itens_per_page": rows_per_page,
+            "prev": pagination.prev_num,
+            "next": pagination.next_num,
+            "current": pagination.page,
+        },
+        "itens": [],
+        "error": False,
+    }
+
+    return data, output
