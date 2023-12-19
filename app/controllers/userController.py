bb9672f19e5edfbc070f2689afd29f94bf8a443b
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-19 09:57:53 -0300

    chore: updating controllers with new logic

--- app/controllers/userController.py
+++ app/controllers/userController.py
@@ -1,12 +1,12 @@
 from app import app, db 
-from flask import request
-from app import User, UserSchema
-from app.controllers import ResponseFactory, request_validator
+from flask import request, jsonify
+from app import User, UserAddSchema, UserEditSchema
 from werkzeug.security import generate_password_hash
+from app.controllers import ResponseFactory, request_validator, paginate, instance_update
 
 
 @app.route("/user/add", methods=["POST"])
-@request_validator(UserSchema)
+@request_validator(UserAddSchema)
 def userAdd():
     data = request.get_json()
     user = User(
@@ -34,16 +34,17 @@ def userAdd():
 
 @app.route("/user/all", methods=["GET"])
 def userAll():
-    users = User.query.all()
-    all = {
-        "users": []
-    }
+    page = request.args.get("page", 1, type=int)
+    rows_per_page = request.args.get("rows_per_page", app.config["ROWS_PER_PAGE"], type=int)
+    userQuery = User.query
+
+    users, output = paginate(userQuery, page, rows_per_page)
 
     for user in users:
         data = user.to_dict()
-        all["users"].append(data)
-
-    return all
+        output["itens"].append(data)
+    
+    return jsonify(output)
 
 
 @app.route("/user/view/<int:id>", methods=["GET"])
@@ -51,29 +52,26 @@ def userView(id: int):
     user = User.query.get(id)
 
     if not user:
-        return ResponseFactory.notFound("User")
+        return ResponseFactory.notFound(f"User {id}")
 
     data = {
         "user": user.to_dict(),
         "error": False
     }
 
-    return data
+    return jsonify(data)
 
 
-@request_validator(UserSchema)
 @app.route("/user/edit/<int:id>", methods=["PUT"])
+@request_validator(UserEditSchema)
 def userEdit(id: int):
     data = request.get_json()
     user = User.query.get(id)
 
     if not user:
-        return ResponseFactory.notFound("User")
-    
-    user.name = data.get("name")
-    user.email = data.get("email")
-    user.password = generate_password_hash(data.get("password"), "scrypt")
+        return ResponseFactory.notFound(f"User {id}")
 
+    instance_update(user, data)
     db.session.add(user)
 
     try:
@@ -93,7 +91,7 @@ def userDelete(id: int):
     user = User.query.get(id)
 
     if not user:
-        return ResponseFactory.notFound("User") 
+        return ResponseFactory.notFound(f"User {id}") 
     
     db.session.delete(user)
 
